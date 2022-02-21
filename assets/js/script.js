0a8dac8cbcef6e553e1f51277debbb338d500f78
hassanidris <hassan.creative@gmail.com>
2022-02-21 22:40:25 +0300

    Fixing hover effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: 59.23607899009092, lng: 17.839677400309043 };
+    const uluru = { lat: 41.12116469545058, lng: -73.43284814453408 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 16,
